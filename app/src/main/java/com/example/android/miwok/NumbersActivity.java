9bc59b42fb5fdfc181fe64bafed02f86783c641e
Abhishek Yadav <abhi.cs.0217@gmail.com>
2019-07-03 14:40:32 +0530

    Changed and reimplemented all the onClick functions with onClickListner interface

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create an array of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        for(int index = 0; index < words.size(); index++){
+            //Create a new TextView
+            TextView wordView = new TextView(this);
+
+            //set the required text
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+
+
+        }
     }
 }
